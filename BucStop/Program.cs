150b839ac379a3f20e573f5c9f75f4aa3c9dffdc
Ethan Frazier <zenedf@users.noreply.github.com>
2024-11-13 00:20:59 -0500

    The only issue now is the index page visitcount not showing updated values during specific circumstances

--- BucStop/Program.cs
+++ BucStop/Program.cs
@@ -43,6 +43,9 @@ builder.Services.AddSingleton<GameService>();
 
 var app = builder.Build();
 
+// Add BaseController to the request pipeline
+app.UseMiddleware<BaseController>();
+
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
@@ -50,9 +53,6 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
-
-// Add BaseController to the request pipeline
-app.UseMiddleware<BaseController>();
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
